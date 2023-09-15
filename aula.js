7cc67123d4be39d458b7262130380629ea12a794
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-15 11:46:57 -0300

    Aula de JS - final

--- aula.js
+++ aula.js
@@ -1,1 +1,21 @@
+// Criando um alerta em javascipt
 alert("E AI MUNDAO");
+
+/*
+
+    **************************
+    - Adicionando javaScript
+    - Criando um novo projeto
+    **************************
+    */ 
+
+    // adicionando uma mensagem na tela
+
+    document.write("Olá, mundo!");
+
+    // Adicionando console.log
+    console.log('Olá Mundo');
+
+    // Criando uma variável
+    let mensagem = "Olá, Mundo";
+    console.log(mensagem)

1fabf4d5c0bfc9110b8a348e98aa8eb12d51f2a5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 11:10:46 -0300

    Subindo exemplos de for

---
+++ exemplo2.js
@@ -0,0 +1,11 @@
+const frutas = ["maça", "banana", "uva", "pera"]
+
+for (let i = 0; i < frutas.length; i++){
+    console.log(`${i} - ${frutas[i]}`);
+}
+
+// for (let i = 0; i in frutas; i++){
+//     if (i == "pera");
+//         console.log('Parei');
+//         break
+// }
